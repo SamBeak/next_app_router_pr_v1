958208bb4e65dc33f58988cd847df7ab2c908189
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-09-30 12:39:45 +0900

    feat: create initial Next.js app structure with index page and styling files

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page(
+    // http://localhost:3000/search?q=가나다라
+    // searchParams는 Promise<>고, q의 string 타입을 가진 객체를 반환하고 있다.
+    {searchParams} : {searchParams : Promise<{q : string}>;}
+) {
+    const { q } = await searchParams;
+    console.log(q); //  가나다라
+  return (
+    <div>search page {q}</div>
+  )
+}
