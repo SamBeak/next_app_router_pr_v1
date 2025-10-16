7893cb24701ce1884d99e1676d3cd934eeec03ff
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-10-16 12:56:35 +0900

    feat: integrate API endpoints for book data fetching and search functionality

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,21 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
 export default async function Page(
     // http://localhost:3000/search?q=가나다라
     // searchParams는 Promise<>고, q의 string 타입을 가진 객체를 반환하고 있다.
-    {searchParams} : {searchParams : Promise<{q : string}>;}
+    {searchParams} : {searchParams : {q?: string;};}
 ) {
-    const { q } = await searchParams;
-    console.log(q); //  가나다라
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`);
+    if (!response.ok) {
+      return <div>에러 발생</div>
+    }
+    const books: BookData[] = await response.json();
   return (
-    <>
-      <div>search page {q}</div>
-    </>
+    <div>
+      {books.map((book : BookData) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
   )
 }
